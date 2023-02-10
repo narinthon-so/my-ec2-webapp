be09383f24dc5afedc06e7faea49e90eb87f4c4e
narinthon sonchaiyaphum <54059270+narinthon-so@users.noreply.github.com>
2023-02-10 11:34:07 +0700

    add features route

--- app/app.py
+++ app/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", title="index")
 
+@app.route("/features")
+def features():
+    return render_template("features.html", title="features")
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
