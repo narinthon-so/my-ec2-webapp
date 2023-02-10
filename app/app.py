5c3e5500aa2adb2afb5c7bceea474290e20da8ae
narinthon sonchaiyaphum <54059270+narinthon-so@users.noreply.github.com>
2023-02-10 15:13:27 +0700

    edit route and integrate with Turnstile

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html", title="index")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="home")
 
 @app.route("/features")
 def features():
