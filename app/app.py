6afd8176551be3331b9845e7794fd660368cec53
narinthon sonchaiyaphum <54059270+narinthon-so@users.noreply.github.com>
2023-02-10 15:15:00 +0700

    add `/` route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return render_template("root.html", title="root")
+
 @app.route("/home")
 def home():
     return render_template("home.html", title="home")
